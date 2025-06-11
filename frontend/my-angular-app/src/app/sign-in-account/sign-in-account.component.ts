999d6295bc7c60a1044b1699905b4b064b1adcf3
Ignotus-21 <rajurajalala@gmail.com>
2025-06-12 01:07:34 +0530

    Add forgot password functionality and alert message to sign-in component

--- frontend/my-angular-app/src/app/sign-in-account/sign-in-account.component.ts
+++ frontend/my-angular-app/src/app/sign-in-account/sign-in-account.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-in-account',
   templateUrl: './sign-in-account.component.html',
-  styleUrls: ['./sign-in-account.component.css']
+  styleUrls: ['./sign-in-account.component.css'],
+  standalone: true,
+  imports: [CommonModule]
 })
-export class SignInAccountComponent {}
+export class SignInAccountComponent {
+  showForgotPasswordMessage = false;
+
+  constructor(private router: Router) {}
+
+  navigateToCreateAccount() {
+    this.router.navigate(['/create-account']);
+  }
+
+  handleForgotPassword() {
+    this.showForgotPasswordMessage = true;
+    // Hide the message after 5 seconds
+    setTimeout(() => {
+      this.showForgotPasswordMessage = false;
+    }, 5000);
+  }
+}
