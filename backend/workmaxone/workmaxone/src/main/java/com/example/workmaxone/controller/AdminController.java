04345648a4832ffb9604d55979b0029796af53f2
kartik sharda <ksharda2700@gmail.com>
2025-06-11 14:57:00 +0530

    Added updated request matchers in jwt

--- backend/workmaxone/workmaxone/src/main/java/com/example/workmaxone/controller/AdminController.java
+++ backend/workmaxone/workmaxone/src/main/java/com/example/workmaxone/controller/AdminController.java
@@ -28,6 +28,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/admin")
 public class AdminController {
 
